71c4cf318d175c71788ebacf3e6478018639a7cd
hengzizng <hengzizng@naver.com>
2018-07-12 19:50:18 +0900

    dd

--- test.c
+++ test.c
@@ -7,10 +7,11 @@ int main() {
 	char str2[20]="hi";
 
 	if(compare(str1,str2)) {
-		printf("%s와 %s는 같은 문장입니다.\n", str1, str2);
+		printf("%s와 %s는 같은 문장입니다.\n", str1, str2)
 	}
 	else {
 		printf("%s와 %s는 다른 문장입니다.\n", str1, str2);
 	}
+	str2[40] = 'a';
 	return 0;
 }
