d4ffd5df53d397d687390b6f1cc4bb5c79861850
hengzizng <hengzizng@naver.com>
2018-07-24 10:32:47 +0900

    code edit

--- test.c
+++ test.c
@@ -5,12 +5,17 @@ char compare(char *str1, char *str2);
 int main() {
 	char str1[20]="hello";
 	char str2[20]="hi";
+	char x;
+	int *ptr;
+
+	*ptr = 50;
+	x = 511;
 
 	if(compare(str1,str2)) {
 		printf("%s and %s are same sentense.\n", str1, str2);
 	}
 	else {
-		printf("%s and %s are different sentense.\n", str1, str2)
+		printf("%s and %s are different sentense.\n", str1, str2);
 	}
 	str2[50] = 'a';
 	return 0;
